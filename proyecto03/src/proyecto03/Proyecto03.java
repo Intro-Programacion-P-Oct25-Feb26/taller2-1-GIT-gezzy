d18e80eabdb36e27a1cbb2005cc7ddd0ce5b0760
GIT-gezzy <jdavichomaldonado@gmail.com>
2025-10-16 12:51:06 -0500

    commit 3-IDE y lenguajes de programacion

--- proyecto03/src/proyecto03/Proyecto03.java
+++ proyecto03/src/proyecto03/Proyecto03.java
@@ -14,7 +14,7 @@ public class Proyecto03 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("El IDE de programacoión es Apache Netbeans y usamos Java y Python como lenguaes de programación");
     }
     
 }
